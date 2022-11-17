87084aa338f64b08a766f7baf2136b9b687dbc7d
Lucas Meurer <lucasmeurer96@gmail.com>
2022-11-17 03:07:22 +0100

    Clean up code, add deviantart source

--- dank-image/src/lib.rs
+++ dank-image/src/lib.rs
@@ -1,53 +1,51 @@
-use std::{collections::HashSet, fs::File, io::Write, path::Path};
+mod utils;
+
+#[macro_use]
+extern crate lazy_static;
+
+use std::{fs::File, io::Write, path::Path};
 
 use dither::{color::palette, prelude::*};
-use image::ImageFormat;
+use image::{
+    imageops::{self, ColorMap, FilterType},
+    ImageBuffer, ImageFormat, Rgb, RgbImage,
+};
 
-pub async fn dither(imageUrl: &str) -> Result<Vec<u8>> {
+lazy_static! {
+    static ref PALETTE: Vec<RGB<u8>> = palette::parse(include_str!("../../palette.plt")).unwrap();
+}
 
-    let img_url = format!("https://img.malmal.io/insecure/w:600/h:448/rt:fit/extend:true/background:FFFFFF/plain/{imageUrl}@png");
-
-    println!("Loading image from: {}", img_url);
-
-    let result = reqwest::get(img_url).await.unwrap();
-
-    let data = result.bytes().await.unwrap();
-
-    println!("Loaded png data: {}", data.len());
-
-    let img = image::load_from_memory_with_format(&data, ImageFormat::Png).unwrap().to_rgb();
-
-
-    println!("Image size {} {}", img.width(), img.height());
+pub async fn dither(image: Vec<u8>) -> Result<Vec<u8>> {
+    let mut img: RgbImage = {
+        let mut target = ImageBuffer::from_pixel(600, 448, Rgb { 0: [255, 255, 255] });
+        let image = image::load_from_memory(&image).unwrap().into_rgb8();
+        utils::fit_image(&mut target, &image);
+        target
+    };
 
     let img = img.pixels().map(|p| RGB::from(p.0));
 
-
-
-    let img: Img<RGB<f64>> = Img::<RGB<u8>>::new(img, 600).unwrap()
+    let img: Img<RGB<f64>> = Img::<RGB<u8>>::new(img, 600)
+        .unwrap()
         .convert_with(|rgb| rgb.convert_with(f64::from));
 
-    let quantize = dither::create_quantize_n_bits_func(4)?;
-    let plt: Vec<_> = palette::parse(include_str!("../../palette.plt")).unwrap();
-
     let dithered_img = ditherer::ATKINSON
-        .dither(img, palette::quantize(plt.as_slice()))
+        .dither(img, palette::quantize(&PALETTE))
         .convert_with(|rgb| rgb.convert_with(clamp_f64_to_u8));
 
-    dithered_img.clone().save(Path::new("preview.png")).unwrap();
+    dithered_img
+        .clone()
+        .save(Path::new("preview.png"))
+        .unwrap();
 
     let img_4bit = dithered_img
         .into_vec()
         .chunks_exact(2)
         .map(|chunk| {
-            plt.iter().position(|col| chunk[1] == *col).unwrap() as u8
-                | ((plt.iter().position(|col| chunk[0] == *col).unwrap() as u8) << 4)
+            ((PALETTE.iter().position(|col| chunk[0] == *col).unwrap() as u8) << 4)
+                | PALETTE.iter().position(|col| chunk[1] == *col).unwrap() as u8
         })
         .collect::<Vec<_>>();
-
-    let mut output = File::create("image.bin").unwrap();
-    output.write_all(img_4bit.as_slice()).unwrap();
-
 
     Ok(img_4bit)
 }
--- dank-server/src/main.rs
+++ dank-server/src/main.rs
@@ -1,5 +1,8 @@
+mod deviantart;
+
+use deviantart::laod_deviantart_iamge;
 use serde::{Deserialize, Serialize};
-use warp::{Filter, reply::Response};
+use warp::{Filter, reply::Response, path::Tail};
 use rand::{Rng, prelude::SliceRandom};
 
 #[tokio::main]
@@ -9,33 +12,37 @@ async fn main() {
     .then(|| async {Response::new(load_malmal_image().await.unwrap().into())});
     let memes = warp::path!("memes")
     .then(|| async {Response::new(load_reddit_meme().await.unwrap().into())});
+    let memes = warp::path!("deviantart")
+    .then(|| async {Response::new(laod_deviantart_iamge().await.unwrap().into())});
+    let url = warp::path("image")
+    .and(warp::path::tail())
+    .then(|tail: Tail| {
+        async move {
+            Response::new(load_url(tail.as_str()).await.unwrap().into())
+        }
+    });
 
-    warp::serve(hello.or(memes))
+    warp::serve(hello.or(memes).or(url))
     .run(([0, 0, 0, 0], 8080))
     .await;
 }
 
 
 async fn load_malmal_image() -> anyhow::Result<Vec<u8>> {
-
     let url = "https://malmal.io/api/gallery/entries?order=TOP&minDate=2021-11-09T22:40:49.187Z&limit=200&offset=0";
-
     let response = reqwest::get(url).await?;
-
     let body = response.text().await?;
 
     let images: Vec<GalleryEntry> = serde_json::from_str(&body)?;
-
     let image = &images[rand::random::<usize>() % images.len()];
 
     println!("Using https://malmal.io/gallery/{}", image.id);
 
-    Ok(dank_image::dither(&image.imageUrl).await?)
+    load_url(&image.imageUrl).await
 }
 
 async fn load_reddit_meme() -> anyhow::Result<Vec<u8>> {
     let subreddit = roux::Subreddit::new("dankmemes+me_irl+meirl");
-
     let hot = subreddit.hot(100, None).await?;
 
     let images = hot.data.children.iter().filter_map(|post| {
@@ -47,8 +54,12 @@ async fn load_reddit_meme() -> anyhow::Result<Vec<u8>> {
     }).collect::<Vec<_>>();
 
     let image = images.choose(&mut rand::thread_rng()).unwrap();
+    load_url(image.url.as_ref().unwrap()).await
+}
 
-    Ok(dank_image::dither(&image.url.as_ref().unwrap()).await?)
+async fn load_url(url: &str) -> anyhow::Result<Vec<u8>> {
+    let image_data = reqwest::get(url).await?.bytes().await?;
+    Ok(dank_image::dither(image_data.into()).await?)
 }
 
 #[derive(Serialize, Deserialize)]
